35fde995ed93067fd4c4dd406a93e48f49b1c2ae
A1nzz <kmamcenko@gmail.com>
2023-02-22 23:32:31 +0300

    added even list function

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,19 +1,41 @@
 import utilities
 
 print("Hello world")
+
+# check calc func
 while True:
     try:
         num1 = int(input("Input first number: "))
         num2 = int(input("Input second number: "))
+        break
     except ValueError:
         print("Incorrect input! Try again: ")
-    else:
-        while True:
-            operation = input("Enter operation(add, sub, mult, div): ")
-            result = utilities.calc(num1, num2, operation)
-            if result != "Incorrect operator":
-                print("Result:", result)
-                break
-            else:
-                print(result)
+
+while True:
+    operation = input("Enter operation(add, sub, mult, div): ")
+    result = utilities.calc(num1, num2, operation)
+    if result != "Incorrect operator":
+        print("Result:", result)
         break
+    else:
+        print(result)
+
+# check even list function
+while True:
+    try:
+        length = int(input("Input length of list: "))
+        break
+    except ValueError:
+        print("Incorrect input! Try again: ")
+
+lst = []
+for i in range(length):
+    while True:
+        try:
+            item = int(input("Input item " + str(i) + " of list: "))
+            lst.append(item)
+            break
+        except ValueError:
+            print("Incorrect input! Try again: ")
+
+print(utilities.get_even_number_list(lst))
--- Lab1/utilities.py
+++ Lab1/utilities.py
@@ -9,3 +9,7 @@ def calc(num1, num2, operation):
         return num1 / num2 if num2 != 0 else "Cannot divided by zero"
     else:
         return "Incorrect operator"
+
+
+def get_even_number_list(lst):
+    return list(item for item in lst if item % 2 == 0)
